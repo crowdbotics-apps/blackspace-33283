b8b9f245e3c4254e48931b6885a01580682523ab
Crowdbotics <team@crowdbotics.com>
2022-02-01 06:00:23 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "blackspace_33283",
+  url: "https://blackspace_33283.botics.co",
+  api: "https://blackspace_33283.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
